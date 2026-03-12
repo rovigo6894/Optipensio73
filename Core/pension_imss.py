dc3ff1f9c961d64b2385033a01d03b550623d689
rovigo6894 <rovigo6894@gmail.com>
2026-03-12 13:14:14 -0600

    Corrección cálculo Ley73 usando tabla IMSS y UMA

--- Core/pension_imss.py
+++ Core/pension_imss.py
@@ -1,83 +1,45 @@
-from Config.parametros import UMA_ACTUAL, CRECIMIENTO_UMA, FACTORES_EDAD
-from Config.tablas_imss import TABLA_CUANTIA
+from Config.parametros import UMA_ACTUAL, FACTORES_EDAD
+from Config.tabla_ley73 import TABLA_LEY73
+import math
 
 
-def obtener_porcentaje_cuantia(salario_uma):
+def buscar_tabla(salario_uma):
 
-    for minimo, maximo, porcentaje in TABLA_CUANTIA:
+    for li, ls, cuantia, incremento in TABLA_LEY73:
+        if salario_uma >= li and salario_uma <= ls:
+            return cuantia, incremento
 
-        if salario_uma >= minimo and salario_uma < maximo:
-            return porcentaje
-
-    return 0.20
+    return 13.00, 2.450
 
 
 def calcular_pension_ley73(
     salario_diario,
-    semanas_cotizadas,
-    edad_actual,
+    semanas,
     edad_retiro,
-    inflacion_anual,
     esposa=False
 ):
 
     salario_uma = salario_diario / UMA_ACTUAL
 
-    porcentaje_cuantia = obtener_porcentaje_cuantia(salario_uma)
+    cuantia_pct, incremento_pct = buscar_tabla(salario_uma)
 
-    cuantia_basica = salario_diario * porcentaje_cuantia
+    cuantia_basica = salario_diario * (cuantia_pct / 100)
 
-    semanas_excedentes = max(semanas_cotizadas - 500, 0)
+    semanas_excedentes = max(semanas - 500,0)
 
-    incremento = (semanas_excedentes / 52) * 0.015
+    incrementos = math.floor(semanas_excedentes / 52)
 
-    pension_base = cuantia_basica * (1 + incremento)
+    aumento = salario_diario * (incremento_pct / 100) * incrementos
 
-    factor_edad = FACTORES_EDAD.get(edad_retiro, 1)
+    pension_diaria = cuantia_basica + aumento
 
-    pension = pension_base * factor_edad
+    factor_edad = FACTORES_EDAD.get(edad_retiro,1)
+
+    pension_diaria *= factor_edad
 
     if esposa:
-        pension *= 1.15
+        pension_diaria *= 1.15
 
-    pension_mensual = pension * 30
+    pension_mensual = pension_diaria * 30
 
-    años_para_retiro = edad_retiro - edad_actual
-
-    pension_futura = pension_mensual * ((1 + inflacion_anual) ** años_para_retiro)
-
-    return pension_mensual, pension_futura
-
-
-def proyectar_pension_anual(
-    pension_inicial,
-    edad_actual,
-    edad_retiro,
-    inflacion
-):
-
-    proyeccion = []
-
-    pension = pension_inicial
-
-    for edad in range(edad_actual, edad_retiro + 1):
-
-        proyeccion.append({
-            "Edad": edad,
-            "Pension_mensual": pension
-        })
-
-        pension = pension * (1 + inflacion)
-
-    return proyeccion
-
-
-def proyectar_uma(uma_actual, crecimiento, años):
-
-    uma = uma_actual
-
-    for i in range(años):
-
-        uma = uma * (1 + crecimiento)
-
-    return uma
+    return pension_mensual
